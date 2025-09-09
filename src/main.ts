dfe4f1ac31d2b0212720413a0576daebbc0d5277
DonizeteVida <donizetevida1998@outlook.com>
2025-09-09 22:27:17 +0200

    Add 'Hello World' sample

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log(process.env.TOKEN)
+import { Client, Events, GatewayIntentBits, GuildChannel } from 'discord.js'
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, client => {
+	console.log(`Ready! Logged in as ${client.user.tag}`)
+
+    const channel = client
+        .channels
+        .cache
+        .filter(channel => (channel as GuildChannel).name == "teste")
+        .first()
+
+    if (channel?.isSendable()) {
+        channel.send("Hello World")
+    }
+});
+
+client.login(process.env.TOKEN)
